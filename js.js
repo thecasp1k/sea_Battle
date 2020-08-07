b8775f54ea1dd4ce9835ed2f7ac39e0416986b82
TheCasp1k <casp1k96@gmail.com>
2020-08-07 17:00:28 +0300

    Backup for home

--- js.js
+++ js.js
@@ -16,15 +16,15 @@ ctx.strokeStyle = '#0703ff';
 ctx.lineWidth = 4;
 ctx.strokeRect(ww*0.1, wh*0.1, 300, 300);
 ctx.strokeRect(ww*0.1+360, wh*0.1, 300, 300);
-ctx.lineWidth = 1;
-for(let i = ww*0.1; i <= 660+ww*0.1; i+= 30){
-    if(i == 114.80000000000001 || i == 414.8 ||  i == 444.8 || i == 474.8 || i == 774.8){
+for(let i = ww*0.1+30; i <= 630+ww*0.1; i+= 30){
+    if(i == ww*0.1+300 ||  i ==  ww*0.1+330 || i == ww*0.1+360 ){
         continue;
     }
     ctx.moveTo(i, wh*0.1-30);
     ctx.lineTo(i, 300+wh*0.1);
 }
 for(let i = wh*0.1; i <= 300+wh*0.1; i+= 30){
+    clog(i)
     if (i == 96.60000000000001 || i == 396.6) {
         continue;
     }
@@ -33,19 +33,59 @@ for(let i = wh*0.1; i <= 300+wh*0.1; i+= 30){
 }
 ctx.font = 'italic 25pt Arial';
 let j = 0;
-for(let i = ww*0.1-90; i <= 300; i+=30){
+for(let i = 30; i <= 300; i+=30){
     if(j == 9) {
         ctx.fillText(arrayNumb[j], ww*0.1-45, wh*0.1+i);
         ctx.fillText(arrayNumb[j], ww*0.1+315, wh*0.1+i);
-        ctx.fillText(arrayLetter[j],ww*0.1+i-20, wh*0.1);
-        ctx.fillText(arrayLetter[j],ww*0.1+i+340, wh*0.1);
+        ctx.fillText(arrayLetter[j],ww*0.1+i-30, wh*0.1);
+        ctx.fillText(arrayLetter[j],ww*0.1+i+330, wh*0.1);
         break;
     }
     ctx.fillText(arrayNumb[j], ww*0.1-30, wh*0.1+i);
     ctx.fillText(arrayNumb[j], ww*0.1+330, wh*0.1+i);
-    ctx.fillText(arrayLetter[j],ww*0.1+i-20, wh*0.1);
-    ctx.fillText(arrayLetter[j],ww*0.1+i+340, wh*0.1);
+    ctx.fillText(arrayLetter[j],ww*0.1+i-30, wh*0.1);
+    ctx.fillText(arrayLetter[j],ww*0.1+i+330, wh*0.1);
     j++;
 }
-ctx.strokeStyle = '#888';
+ctx.strokeStyle = '#000';
+ctx.lineWidth = 2;
 ctx.stroke();
+
+let arrayShips = [4,3,3,2,2,2,1,1,1,1];
+
+// ctx.strokeStyle = '#0703ff';
+// ctx.lineWidth = 3;
+// ctx.strokeRect(ww*0.1, wh*0.1+350, 30, 30);
+// ctx.strokeRect(ww*0.1+30, wh*0.1+350, 30, 30);
+// ctx.strokeRect(ww*0.1+60, wh*0.1+350, 30, 30);
+// ctx.strokeRect(ww*0.1+90, wh*0.1+350, 30, 30);
+
+// ctx.strokeRect(ww*0.1, wh*0.1+385, 30, 30);
+// ctx.strokeRect(ww*0.1+30, wh*0.1+385, 30, 30);
+// ctx.strokeRect(ww*0.1+60, wh*0.1+385, 30, 30);
+// ctx.strokeRect(ww*0.1, wh*0.1+420, 30, 30);
+// ctx.strokeRect(ww*0.1+30, wh*0.1+420, 30, 30);
+// ctx.strokeRect(ww*0.1+60, wh*0.1+420, 30, 30);
+
+// ctx.strokeRect(ww*0.1, wh*0.1+455, 30, 30);
+// ctx.strokeRect(ww*0.1+30, wh*0.1+455, 30, 30);
+// ctx.strokeRect(ww*0.1, wh*0.1+490, 30, 30);
+// ctx.strokeRect(ww*0.1+30, wh*0.1+490, 30, 30);
+// ctx.strokeRect(ww*0.1, wh*0.1+525, 30, 30);
+// ctx.strokeRect(ww*0.1+30, wh*0.1+525, 30, 30);
+
+// ctx.strokeRect(ww*0.1, wh*0.1+560, 30, 30);
+// ctx.strokeRect(ww*0.1, wh*0.1+595, 30, 30);
+// ctx.strokeRect(ww*0.1, wh*0.1+630, 30, 30);
+// ctx.strokeRect(ww*0.1, wh*0.1+665, 30, 30);
+
+canvas.addEventListener('click', (e) => {
+    ctx.beginPath();
+    ctx.strokeStyle = '#888';
+    ctx.lineWidth = 2;
+    ctx.moveTo(474.8, 98.6);
+    ctx.lineTo(504.8, 126);
+    ctx.moveTo(504.8, 98.6);
+    ctx.lineTo(474.8, 126.6);
+    ctx.stroke();
+});
